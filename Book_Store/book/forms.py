1be9c1dc3568235524176a0fac40425dd19b966a
Md. Selim Reza <107039736+SelimBD53@users.noreply.github.com>
2024-06-09 15:38:30 +0600

    first
    this is the first commit

--- Book_Store/book/forms.py
+++ Book_Store/book/forms.py
@@ -4,4 +4,4 @@ from book.models import BookStoreModel
 class BookStoreForm(forms.ModelForm):
     class Meta:
        model = BookStoreModel
-       exclude = ['first_pub', 'last_pub']
+       fields = ['id', 'book_name', 'author', 'category']
--- Book_Store/book/urls.py
+++ Book_Store/book/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from book.views import home
+from book.views import home, store_book, show_books, edit_book
 
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path('store_new_book/', store_book, name="store_book"),
+    path('show_books/', show_books, name="show_books"),
+    path('edit_book/<int:id>', edit_book, name="edit_book"),
+
 ]
