020ed46fc8fd758632c8d2ac2ee6f7663b68d348
Md. Selim Reza <107039736+SelimBD53@users.noreply.github.com>
2024-06-10 14:41:49 +0600

    Delete
    in this project delete successfully

--- Book_Store/book/models.py
+++ Book_Store/book/models.py
@@ -15,6 +15,6 @@ class BookStoreModel(models.Model):
    book_name = models.CharField(max_length=30)
    author = models.CharField(max_length=30)
    category = models.CharField(max_length=30, choices=CATEGORY)
-   first_pub = models.DateTimeField(auto_now_add=True)
-   last_pub = models.DateTimeField(auto_now=True)
+   first_pub = models.DateTimeField(auto_now_add=True)   # django ekdom first date
+   last_pub = models.DateTimeField(auto_now=True)   # erpor joto update korbo sei date dekhabe
     
